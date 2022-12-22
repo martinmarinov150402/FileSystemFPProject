99a86581bbc0d20d383734332e0552c13e8d07a8
Martin Marinov <martinmarinov150402@gmail.com>
2022-12-23 01:09:27 +0200

    Halfly implemented cd

--- tree.hs
+++ tree.hs
@@ -1,9 +1,10 @@
 import Data.List
+import Data.List.Split
 import System.IO
 
 
 data Tree a = EmptyNode
-            | Node a [Tree a]
+            | Node a [Tree a] 
             deriving (Eq, Show)
 
 printtree :: Tree String -> String
@@ -21,6 +22,19 @@ rootT (Node a _) = a
 testTree :: Tree String
 testTree = Node "/" [Node "etc" [], Node "mnt" [], Node "home" []]
 
+fileSystem :: Tree String
+fileSystem = testTree
+
+pwd = putStrLn $ rootT fileSystem
+
+cd :: String -> Tree String 
+cd "" = error "Empty path"
+cd a = cdUtil (splitOn "/" a) (Just fileSystem)
+
+cdUtil :: [String] ->  (Maybe (Tree String)) -> Tree String
+cdUtil _ Nothing = error "Invalid path"
+cdUtil [] (Just fs) = fs
+cdUtil (x:xs) (Just (Node a trees)) = cdUtil xs (find (\ tree -> (rootT tree) == x) trees)
 
 getNodeTree :: Tree a -> [Tree a]
 getNodeTree EmptyNode = error "Empty"
