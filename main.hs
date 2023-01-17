171fb14c765a47e2e33723492de3bafd2d2de029
Martin Marinov <martinmarinov150402@gmail.com>
2023-01-17 13:57:59 +0200

    It finally works

--- main.hs
+++ main.hs
@@ -1,5 +1,6 @@
 import System.IO
 import Data.List.Split
+import Data.Char
 import Text.Show.Functions
 
 data Node = Dir String | File String String deriving Show
@@ -14,11 +15,19 @@ data IntToNodeMap = INRecord Int Node
 fsIntToNodeMapInitial :: [IntToNodeMap]
 fsIntToNodeMapInitial = [(INRecord 0 (Dir "/")), (INRecord 1 (Dir "etc")), (INRecord 2 (Dir "mnt")), (INRecord 3 (Dir "home")), (INRecord 4 (File "bativan.txt" "Bai Ivan")), (INRecord 5 (File "eqk.txt" " e mnogo qk"))]
 
+-- 0 1 / 1 1 etc 2 1 mnt 3 1 home 4 2 bativan.txt Bai Ivan 5 2 eqk.txt e mnogo qk
+
 fsParrentListInitial :: [ParrentList]
 fsParrentListInitial = [(PLRecord 0 0), (PLRecord 1 0), (PLRecord 2 0), (PLRecord 3 0), (PLRecord 4 1), (PLRecord 5 1)]
 
+-- 0 0 1 0 2 0 3 0 4 1 5 1
+
 fsAdjacencyListInitial :: [AdjacencyList]
 fsAdjacencyListInitial = [(ALRecord 0 [1, 2, 3]), (ALRecord 1 [4, 5]), (ALRecord 2 []), (ALRecord 3 [])]
+
+-- 0 1,2,3 1 4,5 2 0,0 3 0,0
+
+
 
 fst3 :: (a, b, c) -> a
 fst3 (f,s,t) = f
@@ -156,6 +165,14 @@ mkfileCmd :: Int -> [String] -> [IntToNodeMap] -> [ParrentList] -> [AdjacencyLis
 mkfileCmd _ [] _ _ _ = putStrLn("This command requires 2 arguments")
 mkfileCmd fs (x:xs) fsIntToNodeMap fsParrentList fsAdjacencyList = if xs == [] then putStrLn("This command requires 2 arguments") else createNewFile fs x (concatAllStringsInListWithSpacesBetween xs) fsIntToNodeMap fsParrentList fsAdjacencyList
 
+saveFs :: [IntToNodeMap] -> [ParrentList] -> [AdjacencyList] -> IO()
+saveFs fsIntToNodeMap fsParrentList fsAdjacencyList = do
+    fileH <- openFile "tree.txt" WriteMode
+    saveINMToFile fileH fsIntToNodeMap
+    savePLToFile fileH fsParrentList
+    saveALToFile fileH fsAdjacencyList
+    hClose fileH
+
 processCommand :: Int -> [String] -> [IntToNodeMap] -> [ParrentList] -> [AdjacencyList] -> IO()
 processCommand _ [] _ _ _ = error "Empty command"
 processCommand fs (x:xs) fsIntToNodeMap fsParrentList fsAdjacencyList = do
@@ -165,10 +182,101 @@ processCommand fs (x:xs) fsIntToNodeMap fsParrentList fsAdjacencyList = do
               "rm" -> inputCommand fs (trd3 res) (fst3 res) (snd3 res) where res = (rm fs xs fsParrentList fsAdjacencyList fsIntToNodeMap)
               "cat" -> catCmdProcess fs xs fsIntToNodeMap fsParrentList fsAdjacencyList
               "mkfile" -> mkfileCmd fs xs fsIntToNodeMap fsParrentList fsAdjacencyList
+              "pal" -> print fsAdjacencyList
+              "ppl" -> print fsParrentList
+              "pinm" -> print fsIntToNodeMap
+              "save" -> saveFs fsIntToNodeMap fsParrentList fsAdjacencyList
               otherwise -> putStrLn "Unknown command"
     inputCommand fs fsIntToNodeMap fsParrentList fsAdjacencyList
 
+stringToInt :: String -> Int
+stringToInt "" = 0
+stringToInt (x:xs) = 10 * (stringToInt xs) + ord x - ord '0'
 
+nElementsForward :: [a] -> Int -> [a]
+nElementsForward [] _ = []
+nElementsForward ls 0 = ls
+nElementsForward (x:xs) n = nElementsForward xs (n-1) 
+
+parsableString :: String -> Bool
+parsableString [] = True
+parsableString (x:xs) = if x >= '0' && x <= '9' then parsableString xs else False
+
+concatUntilItIsntParsable :: [String] -> String
+concatUntilItIsntParsable [] = ""
+concatUntilItIsntParsable (x:xs) = if parsableString x then "" else x ++ " " ++ concatUntilItIsntParsable xs
+
+getFirstParsable :: [String] -> [String]
+getFirstParsable [] = []
+getFirstParsable (x:xs) = if parsableString x then (x:xs) else getFirstParsable xs
+
+getInMapFromListOfStr :: [String] -> [IntToNodeMap] -> [IntToNodeMap]
+getInMapFromListOfStr [] fsIntToNodeMap = fsIntToNodeMap
+getInMapFromListOfStr ls fsIntToNodeMap = if(kind == 1) then getInMapFromListOfStr (nElementsForward ls 3) ((INRecord id (Dir name)) : fsIntToNodeMap) else getInMapFromListOfStr (getFirstParsable (nElementsForward ls 3)) ((INRecord id (File name (concatUntilItIsntParsable (nElementsForward ls 3)))) : fsIntToNodeMap)  where id = stringToInt $ getHeadFromList ls
+                                                                                                                                                                                                                                                                                                                                     kind = stringToInt $ getHeadFromList (nElementsForward ls 1)
+                                                                                                                                                                                                                                                                                                                                     name = getHeadFromList(nElementsForward ls 2)
+
+getIntToNodeMapFromString :: String -> [IntToNodeMap]
+getIntToNodeMapFromString str = getInMapFromListOfStr (splitOn " " str) []
+
+getPLFromListofString :: [String] -> [ParrentList] -> [ParrentList]
+getPLFromListofString [] fsParrentList = fsParrentList
+getPLFromListofString (x:xs) fsParrentList = getPLFromListofString (nElementsForward xs 1) ((PLRecord (stringToInt x) (stringToInt (getHeadFromList xs))) : fsParrentList) 
+
+getParrentListFromString :: String -> [ParrentList]
+getParrentListFromString str = getPLFromListofString (splitOn " " str) []
+
+getListFromString :: String -> [Int]
+getListFromString "0,0" = []
+getListFromString str = map stringToInt (splitOn "," str)
+
+getALFromListofString :: [String] -> [AdjacencyList] -> [AdjacencyList]
+getALFromListofString [] fsAdjacencyList = fsAdjacencyList
+getALFromListofString (x:xs) fsAdjacencyList = getALFromListofString (nElementsForward xs 1) ((ALRecord (stringToInt x) (getListFromString (getHeadFromList xs))) : fsAdjacencyList)
+
+getALFromString :: String -> [AdjacencyList]
+getALFromString str = getALFromListofString (splitOn " " str) []
+
+plToString :: [ParrentList] -> String
+plToString [] = ""
+plToString (x:xs) = if isEmpty xs then show (getIdFromPLRecord x) ++ " " ++ show (decomposePLRecord x) else show (getIdFromPLRecord x) ++ " " ++ show (decomposePLRecord x) ++ " " ++ plToString xs
+
+isEmpty :: [a] -> Bool
+isEmpty [] = True
+isEmpty (x:xs) = False
+
+
+inmToString :: [IntToNodeMap] -> String
+inmToString [] = ""
+inmToString (x:xs) = case node of (Dir _) -> if isEmpty xs then show (getIdFromINRecord x) ++ " 1 " ++ getNameFromNode node else show (getIdFromINRecord x) ++ " 1 " ++ getNameFromNode node ++ " " ++ inmToString xs
+                                  otherwise -> show (getIdFromINRecord x) ++ " 2 " ++ getNameFromNode node ++ " " ++ (getFileContent node) ++ inmToString xs 
+                     where node = decomposeINRecord x
+
+alLToString :: [Int] -> String
+alLToString [] = "0,0"
+alLToString ls = listToString ls
+
+strip = lstrip . rstrip
+lstrip = dropWhile (`elem` " \t")
+rstrip = reverse . lstrip . reverse
+
+
+listToString :: [Int] -> String
+listToString [] = ""
+listToString (x:xs) = if(xs == []) then (show x) else (show x) ++ "," ++ listToString xs 
+
+alToString :: [AdjacencyList] -> String
+alToString [] = ""
+alToString (x:xs) = if isEmpty xs then show (getIdFromAlRecord x) ++ " " ++ alLToString (decomposeAlRecord x)  else show (getIdFromAlRecord x) ++ " " ++ alLToString (decomposeAlRecord x) ++ " " ++ alToString xs
+
+savePLToFile :: Handle -> [ParrentList] -> IO()
+savePLToFile fileH fsParrentList = hPutStrLn fileH (plToString fsParrentList)
+
+saveINMToFile :: Handle -> [IntToNodeMap] -> IO()
+saveINMToFile fileH fsIntToNodeMap = hPutStrLn fileH (inmToString fsIntToNodeMap)
+
+saveALToFile :: Handle -> [AdjacencyList] -> IO()
+saveALToFile fileH fsAdjacencyList = hPutStrLn fileH (alToString fsAdjacencyList)
 
 inputCommand :: Int -> [IntToNodeMap] -> [ParrentList] -> [AdjacencyList] -> IO()
 inputCommand fs fsIntToNodeMap fsParrentList fsAdjacencyList = do
@@ -176,4 +284,10 @@ inputCommand fs fsIntToNodeMap fsParrentList fsAdjacencyList = do
      let args = splitOn " " cmd
      processCommand fs args fsIntToNodeMap fsParrentList fsAdjacencyList
 main = do
-    inputCommand 0 fsIntToNodeMapInitial fsParrentListInitial fsAdjacencyListInitial
+    readTreeFile <- openFile "tree.txt" ReadMode
+    inmapLine <- hGetLine readTreeFile
+    plLine <- hGetLine readTreeFile
+    alLine <- hGetLine readTreeFile
+    hClose readTreeFile
+
+    inputCommand 0 (getIntToNodeMapFromString (strip inmapLine)) (getParrentListFromString (strip plLine)) (getALFromString (strip alLine))
